1d3d88f56e36a6abe0cefdcf58a1434faed04f9c
arieldiazing93-sys <arieldiazing93@gmail.com>
2026-08-21 14:04:44 -0300

    Delete tumenusmart/src/app/admin/logout/actions.ts

--- tumenusmart/src/app/admin/logout/actions.ts
+++
@@ -1,9 +0,0 @@
-"use server";
-
-import { redirect } from "next/navigation";
-import { cerrarSesionAdmin } from "@/lib/auth";
-
-export async function cerrarSesion() {
-  await cerrarSesionAdmin();
-  redirect("/admin/login");
-}
