ad85eb5e874178d5e77b4b0acbfa80a7821a40e2
arieldiazing93-sys <arieldiazing93@gmail.com>
2026-08-22 12:08:53 -0300

    Delete tumenusmart/Downloads directory

--- tumenusmart/Downloads/Actualizaciones TuMenuSmart/tumenusmart/src/lib/format.ts
+++
@@ -1,8 +0,0 @@
-export function formatearGuarani(valor: number | string) {
-  const numero = typeof valor === "string" ? parseFloat(valor) : valor;
-  return new Intl.NumberFormat("es-PY", {
-    style: "currency",
-    currency: "PYG",
-    maximumFractionDigits: 0,
-  }).format(numero);
-}
